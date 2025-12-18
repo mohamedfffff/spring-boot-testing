b99004e99966aaf57ffc626f4c1868f6dd0fffea
the thing <65288778+mohamedfffff@users.noreply.github.com>
2025-12-18 12:14:03 +0200

    finished project, waiting deployment

--- src/test/java/com/example/spring_boot_testing/ItemServiceIntegrationTest.java
+++ src/test/java/com/example/spring_boot_testing/ItemServiceIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.example.spring_boot_testing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
